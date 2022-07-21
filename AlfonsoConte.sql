148877a9e428cbf55787a5e57fae7e378fa4f222
Alfonso Conte <56754654+AlfonsoConte@users.noreply.github.com>
2022-07-21 12:44:18 +0200

    Add files via upload

---
+++ AlfonsoConte.sql
@@ -0,0 +1,2 @@
+create user C##Covid identified by 1234;
+grant dba,unlimited tablespace to C##Covid;
---
+++ Covid_Tesina.sql
@@ -0,0 +1,11 @@
+alter table Regioni
+add Ricorsi_PS integer 
+
+
+
+
+select * from regioni
+order by denominazione_Regione
+
+update regioni
+set ricorsi_ps=ricorsi_ps*1000;
